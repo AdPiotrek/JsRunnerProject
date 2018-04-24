0198d82ce075d384ea4f993b001da95b94680dd7
padamus <piotrekadamus16@gmail.com>
2018-04-24 17:53:07 +0200

    Simply runner added.

--- pkRunner.js
+++ pkRunner.js
@@ -1,5 +1,4 @@
 class Game {
-
     constructor() {
         this.canvas = document.createElement('canvas');
         this.ctx = this.canvas.getContext('2d');
@@ -14,39 +13,67 @@ class Game {
         this.background.src = './assets/images/background.png';
         this.background.onload = () => {
             this.ctx.drawImage(this.background, 0, 0);
+            this.anim();
         };
-        this.player = {};
-        this.player.width  = 60;
-        this.player.height = 96;
-        this.player.speed  = 6;
-        this.player.sheet  = new SpriteSheet('assets/images/runner.png', player.width, player.height);
-        this.player.anim   = new Animation(player.sheet, 4, 0, 15);
+        this.spreadSheet = new SpriteSheet('assets/images/runner.png', 6, 4);
+        this.playerAnim = new Animation(this.spreadSheet, 4, this.ctx);
     };
 
-
+    anim() {
+        const animation = () => {
+            console.log(this);
+            this.ctx.clearRect(0,0, 400,400);
+            this.playerAnim.draw();
+            requestAnimationFrame(animation);
+        };
+        animation();
+    };
 }
-
-new Game();
 
 class SpriteSheet {
 
-    constructor(path, frameWidth, frameHeight) {
+    constructor(path, columns, rows) {
         this.image = new Image();
-        this.frameWidth = frameWidth;
-        this.frameHeight = frameHeight;
+        this.image.src = path;
+        this.columns = columns;
+        this.rows = rows;
+        this.size = rows * columns;
 
         this.image.onload = () => {
-            this.framesPerRow = Math.floor(this.image.width / this.frameWidth);
+            this.frameHeight = this.image.naturalHeight / rows;
+            this.frameWidth = this.image.naturalWidth / columns;
         };
-
-        this.image.src = path;
     };
 
 }
 
 class Animation {
 
-    constructor(spritesheet, frameSpeed, startFrame, endFrame){
-        
+    constructor(spritesheet, frameSpeed, ctx) {
+        this.spritesheet = spritesheet;
+        this.frameSpeed = frameSpeed;
+        this.currentFrame = 0;
+        this.ctx = ctx;
     }
+
+    draw() {
+        const row = Math.floor(this.currentFrame / this.spritesheet.columns);
+        const column = this.currentFrame % this.spritesheet.columns;
+        this.ctx.drawImage(
+            this.spritesheet.image,
+            column * this.spritesheet.frameWidth,
+            row * this.spritesheet.frameHeight,
+            this.spritesheet.frameWidth,
+            this.spritesheet.frameHeight,
+            40, 40,
+            this.spritesheet.frameWidth,
+            this.spritesheet.frameHeight
+        );
+        this.currentFrame++;
+        this.currentFrame = this.currentFrame % this.spritesheet.size;
+    }
+
 }
+
+
+new Game();
