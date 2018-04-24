875ef0fffacb031c01fb1206d9e6a874963e7011
root <root@DESKTOP-D2IGVE4.localdomain>
2018-04-24 15:15:31 +0200

    Fit background size

--- pkRunner.js
+++ pkRunner.js
@@ -3,11 +3,9 @@ class Game {
     constructor() {
         this.canvas = document.createElement('canvas');
         this.ctx = this.canvas.getContext('2d');
-        canvas = document.createElement('canvas');
-        let context = canvas.getContext('2d');
-        canvas.width = document.documentElement.clientWidth;
-        canvas.height = document.documentElement.clientHeight - 5;
-        document.body.appendChild(canvas);
+        this.canvas.width = document.documentElement.clientWidth;
+        this.canvas.height = document.documentElement.clientHeight - 5;
+        document.body.appendChild(this.canvas);
         this.init();
     }
 
@@ -16,7 +14,13 @@ class Game {
         this.background.src = './assets/images/background.png';
         this.background.onload = () => {
             this.ctx.drawImage(this.background, 0, 0);
-        }
+        };
+        this.player = {};
+        this.player.width  = 60;
+        this.player.height = 96;
+        this.player.speed  = 6;
+        this.player.sheet  = new SpriteSheet('assets/images/runner.png', player.width, player.height);
+        player.anim   = new Animation(player.sheet, 4, 0, 15);
     };
 
 
@@ -24,3 +28,18 @@ class Game {
 
 new Game();
 
+class SpriteSheet {
+
+    constructor(path, frameWidth, frameHeight) {
+        this.image = new Image();
+        this.frameWidth = frameWidth;
+        this.frameHeight = frameHeight;
+
+        this.image.onload = () => {
+            this.framesPerRow = Math.floor(this.image.width / this.frameWidth);
+        };
+
+        this.image.src = path;
+    };
+
+}
