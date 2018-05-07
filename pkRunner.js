62029850822bcd325cacc9a4c5693eac9f8639dc
root <root@DESKTOP-D2IGVE4.localdomain>
2018-05-07 18:45:17 +0200

    add many things

--- pkRunner.js
+++ pkRunner.js
@@ -13,8 +13,8 @@ class Game {
         this.canvas.height = document.documentElement.clientHeight - 5;
         document.body.appendChild(this.canvas);
         platformBase = this.canvas.height - platformWidth;
-        this.ground = [];
         this.frame = 0;
+        this.lifes = 0;
         this.points = 0;
         this.gapeLength = 0;
         this.platformLength = 0;
@@ -29,20 +29,25 @@ class Game {
     }
 
     init() {
+        this.ground = [];
+        this.hearths = [];
         this.frame = 0;
-        this.points = 0;
+        if(this.lifes === -1) {
+            this.points = 0;
+            this.lifes = 0;
+        }
         this.spreadSheet = new SpriteSheet('assets/images/runner.png', 8, 1);
         this.player = new Player();
         this.player.anim = new Animation(this.spreadSheet, this.ctx);
         this.bgAnim = new ScrollAnimation(this.background, this.ctx, 5);
         this.isGameRunning = true;
-
+        this.initGround();
     };
 
     initGround() {
         for (let i = 0; i < 6 + this.platformLength; i++) {
             this.ground.push(
-                new Block(i * platformWidth, platformBase - platformSpacer, this.player, this.ctx)
+                new Block(i * platformWidth, platformBase - platformSpacer,'', this.player, this.ctx)
             )
         }
 
@@ -62,6 +67,11 @@ class Game {
             }
         }
 
+        for (let i = 0; i < this.hearths.length; i++) {
+            this.hearths[i].update();
+            this.hearths[i].draw();
+        }
+
         // remove ground that have gone off screen
         if (this.ground[0] && this.ground[0].x < -platformWidth) {
             this.ground.splice(0, 1);
@@ -75,15 +85,18 @@ class Game {
                 this.frame++;
                 if(this.frame % 50 === 0){
                     this.points++;
+                    this.frame = 0;
                 }
                 animFrame++;
                 this.drawPoints();
+                this.drawLifes();
                 this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
                 this.bgAnim.draw();
                 this.player.update();
                 this.player.anim.draw(animFrame, this.player.x, this.player.y);
                 this.updateGround();
-                if (this.ground.length < 15000) {
+                this.lifes = this.player.checkCollisionWithHearth(this.hearths) === true ? this.lifes = this.lifes + 1 : this.lifes;
+                if (this.ground.length < 5000) {
                     this.spawnBlock();
                 }
                 this.gameOver();
@@ -99,11 +112,17 @@ class Game {
         const level = Math.floor(Math.random() * 3);
         this.platformLength = Math.floor(Math.random() * 4) + 2;
         const arrLeng = this.ground.length + 1;
-        this.gapeLength = Math.floor(Math.random() * 2) + 2;
+        this.gapeLength = Math.floor(Math.random() * 2 + 1) + 2;
+        let hearthAdded = false;
         for (let i = arrLeng; i < arrLeng + this.platformLength; i++) {
+            const hearthRandom = Math.floor(Math.random() * 100);
             this.ground.push(
                 new Block((this.gapeLength + i) * platformWidth, platformBase - platformSpacer * level, '', this.player, this.ctx)
-            )
+            );
+            if(hearthRandom %  60 === 0 && !hearthAdded) {
+                hearthAdded = true;
+                this.hearths.push(new Hearth((this.gapeLength + i) * platformWidth + 32, platformBase - platformSpacer * (level + 1) + 25, '', this.player, this.ctx))
+            }
         }
 
 
@@ -117,7 +136,10 @@ class Game {
 
     gameOver() {
         if (this.player.y > this.canvas.height) {
-
+            this.lifes--;
+            if(this.lifes >= 0 ){
+                return this.init();
+            }
             this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
             this.isGameRunning = false;
 
@@ -125,11 +147,15 @@ class Game {
             const btn = document.getElementById('gameOverButton');
             const result = document.getElementById('result');
             const score = document.getElementById('score');
+            const lifes = document.getElementById('lifes');
             score.style.visibility = 'hidden';
             btn.style.visibility = 'visible';
             result.style.color = 'Red';
+            lifes.style.color = 'Red';
             result.innerHTML = "Your score: " + this.points.toString();
+            lifes.innerHTML = this.lifes === -1 ? "Lifes: " + 0 : "Lifes: " + this.lifes.toString();
             result.style.visibility = 'visible';
+            lifes.style.visibility = 'visible';
 
             btn.addEventListener('click', () => {
                 score.style.visibility = 'visible';
@@ -148,6 +174,14 @@ class Game {
         score.style.top = 0 + "px";
         score.style.color = 'Red';
         score.innerHTML = "Actual score: " + this.points.toString();
+    }
+
+    drawLifes(){
+        const score= document.getElementById('lifes');
+        score.style.left = 400 + "px";
+        score.style.top = 0 + "px";
+        score.style.color = 'Red';
+        score.innerHTML = "Lifes: " + this.lifes.toString();
     }
 }
 
@@ -266,7 +300,7 @@ class Player extends Vectors {
         this.jumpCounter = 0;
         this.speed = 10;
         this.height = 100;
-        this.width = 100;
+        this.width = 90;
     }
 
     //dziedzicząca z vektor
@@ -288,6 +322,27 @@ class Player extends Vectors {
         if (this.isFalling || this.isJumping) {
             this.dy = Math.min(this.dy + 1, 10);
         }
+    }
+
+    checkCollisionWithHearth(hearths){
+        const playerLeft= this.x;
+        const playerRight = this.x + this.width;
+        const playerTop = this.y;
+        const playerBottom = this.y + this.height;
+        for(let i=0 ; i < hearths.length ; i++){
+            const hearthLeft = hearths[i].x;
+            const hearthRight = hearths[i].x + hearths[i].width;
+            const hearthTop = hearths[i].y;
+            const hearthBottom = hearths[i].y + hearths[i].height;
+            if(!(hearthLeft > playerRight || hearthRight < playerLeft ||
+                hearthTop > playerBottom ||  hearthBottom < playerTop)){
+                this.sound = new Audio('./assets/sound/coin.mp3');
+                this.sound.play();
+                hearths.splice(i,1);
+                return true;
+            }
+        }
+        return false;
     }
 
     reset() {
@@ -319,6 +374,33 @@ class Block extends Vectors {
 
     draw() {
         this.ctx.drawImage(this.image, this.x, this.y, 100, 100);
+    }
+
+
+}
+
+class Hearth extends Vectors {
+    constructor(x, y, type, relativePlayer, ctx) {
+        super(x, y);
+        this.width = platformWidth;
+        this.height = platformSpacer;
+        this.player = relativePlayer;
+        this.ctx = ctx;
+        this.newImg();
+    }
+
+    newImg() {
+        this.image = new Image();
+        this.image.src = 'assets/images/hearth.png'
+    }
+
+    update() {
+        this.dx = -this.player.speed;
+        this.advance();
+    }
+
+    draw() {
+        this.ctx.drawImage(this.image, this.x, this.y, 40,40);
     }
 
 
